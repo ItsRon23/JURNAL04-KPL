1ab51d9a34404f16e5144fb7be5b2d12f9fed125
ItsRon23 <aaronnomor2@gmail.com>
2024-03-13 13:11:39 +0700

    menambahkan class kodebuah

--- Program.cs
+++ Program.cs
@@ -1,23 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static KodeBuah;
 
-public class kodeBuah
+public class KodeBuah
 {
-    public enum EnumKodepos
+    public enum EnumBuah
     {
         Apel, Aprikot, Alpukat, 
         Pisang, Paprika, 
         Blackberry, 
         Ceri, 
         Kelapa,
-        Jagung
-    }
-
-    public static String getKodeBuah(EnumKodepos kodeBuah)
+        Jagung,
+        Kurma,
+        Durian,
+        Anggur,
+        Melon,
+        Semangka
+    } 
+    
+    public static String getKodeBuah(EnumBuah buah)
     {
-        String[] outputKode = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00"};
-        return outputKode[(int)kodeBuah];
+        String[] output = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00"};
+        return output[(int)buah];
     }
 }
 
+public class mainClass
+{
+    public static void Main(String[] args)
+    {
+        EnumBuah list = EnumBuah.Durian;
+        String kodeBuah = getKodeBuah(list);
+        Console.WriteLine("Buah {1}, kode Buahnya adalah {0}", kodeBuah, list);
+
+    }
 }
+
