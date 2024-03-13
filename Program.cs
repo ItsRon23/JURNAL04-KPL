22776919c80a94b8b761c04cba12a9c8a3d783f4
ItsRon23 <aaronnomor2@gmail.com>
2024-03-13 13:03:46 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class kodeBuah
+{
+    public enum EnumKodepos
+    {
+        Apel, Aprikot, Alpukat, 
+        Pisang, Paprika, 
+        Blackberry, 
+        Ceri, 
+        Kelapa,
+        Jagung
+    }
+
+    public static String getKodeBuah(EnumKodepos kodeBuah)
+    {
+        String[] outputKode = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00"};
+        return outputKode[(int)kodeBuah];
+    }
+}
+
+}
